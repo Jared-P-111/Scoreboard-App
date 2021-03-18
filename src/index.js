8f80b90287c9e5a1af34843fdd87d52766c2bbf8
Jared_P <jaredpisell@gmail.com>
2021-03-17 17:14:37 -0700

    Latest rev after adding state for inc and decrement buttons.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,109 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import './app.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//The object we are using for data
+const players = [
+  {
+    name: 'Jared',
+    score: '50',
+    id: 1,
+  },
+  {
+    name: 'Janice',
+    score: '30',
+    id: 2,
+  },
+  {
+    name: 'Samantha',
+    score: 49,
+    id: 3,
+  },
+  {
+    name: 'Lisa',
+    score: 5,
+    id: 4,
+  },
+  {
+    name: 'Franker',
+    score: 22,
+    id: 5,
+  },
+  {
+    name: 'Austin',
+    score: 33,
+    id: 6,
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//
+const Header = (props) => {
+  return (
+    <header>
+      {console.log(props)}
+      <h1>{props.title}</h1>
+      <span className="stats">Players: {props.totalPlayers}</span>
+    </header>
+  );
+};
+
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      score: 0,
+    };
+  }
+
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score + 1,
+    });
+  };
+
+  decrementScore = () => {
+    this.setState({
+      score: this.state.score - 1,
+    });
+  };
+
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore}>
+          -
+        </button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment" onClick={this.incrementScore}>
+          +
+        </button>
+      </div>
+    );
+  }
+}
+
+const Player = (props) => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        {props.playerKey}: {props.playerName}
+      </span>
+      <Counter />
+    </div>
+  );
+};
+
+const App = (props) => {
+  return (
+    <div className="scoreboard">
+      <Header title="Scoreboard" totalPlayers={props.initialPlayers.length} />
+      {props.initialPlayers.map((
+        player //Remember use the player object!
+      ) => (
+        <Player playerName={player.name} playerKey={player.id} />
+      ))}
+    </div>
+  );
+};
+
+ReactDOM.render(<App initialPlayers={players} />, document.getElementById('root'));
